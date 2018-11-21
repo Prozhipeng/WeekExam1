3766947047146f4a47f85a5809d869b360f68c16
Prozhipeng <44663810+Prozhipeng@users.noreply.github.com>
2018-11-21 08:40:42 +0800

    Add files via upload

--- 11.20.php
+++ 11.20.php
@@ -1,18 +1,24 @@
 <?php
-
-function NumberOf1($n)
-{
- 	$count = 0;
-  	$flag = 1;
-  	while ($flag != 0) {
-	  	if (($n & $flag) != 0) {
-	   		 $count++;
-	    }
-	   	$flag = $flag << 1;
-  	}
-  	return $count;
+//递归方式
+function bin_recur_search($arr,$val){
+    global $time;
+    if(count($arr) >= 1){
+        $mid = intval(count($arr) / 2);
+        $time++;
+        if($arr[$mid] == $val){
+            $key = array_keys($arr,$arr[$mid]);
+            return '值为:'.$arr[$mid].'<br>查找次数：'.$time.'<br>'.'下标为'.$key;
+        }elseif($arr[$mid] > $val){
+            $arr = array_splice($arr,0,$mid);
+            return bin_recur_search($arr, $val);
+        }else{
+            $arr = array_slice($arr,$mid + 1);
+            return bin_recur_search($arr, $val);
+        }
+    }
+    return '未找到'.$val;
 }
 
-$num=45;
-echo $num."的二进制是".decbin($num)."<br/>";
-echo $num."共有".NumberOf1($num)."个1";
+$arr = array(1,3,5,7,13,19,25,68,98,145,673,8542);
+$val = 145;
+print_r(bin_recur_search($arr,$val));
